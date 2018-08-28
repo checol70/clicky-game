1c0be3e92b39773e47dae7fba7e100d83a2dfe54
Sheckerol <checol70@gmail.com>
2018-08-27 21:17:04 -0700

    maybe fixed images

--- src/components/ClickyItem/ClickyItem.js
+++ src/components/ClickyItem/ClickyItem.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import require from "require"
 const style = {width:"100%", margin:"30px auto"};
 //stupid thing needs a comment to commit.
 const ClickyItem =(props)=>(
-       <img src={props.src}onClick={props.onClick}  className= "text-center" alt={props.alt} style={style}/> 
+       <img src={require(props.src)}onClick={props.onClick}  className= "text-center" alt={props.alt} style={style}/> 
 )
 
 export default ClickyItem
